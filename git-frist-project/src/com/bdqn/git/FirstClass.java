282c1440253bc9c21a76267b4371ec3340c6c087
18581023664 <451769267@qq.com>
2019-07-21 14:20:06 +0800

    修改firstClass

--- git-frist-project/src/com/bdqn/git/FirstClass.java
+++ git-frist-project/src/com/bdqn/git/FirstClass.java
@@ -9,11 +9,16 @@ package com.bdqn.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello  xxxx");
+        System.out.println("hello  xxxx");
+        System.out.println("hello  xxxx");
+        //hello test
     }
 
     public void test(){
         int i=0;
         i+=10;
+
+        System.out.println(i);
     }
 
 }
