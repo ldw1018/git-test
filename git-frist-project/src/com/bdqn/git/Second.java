b323ce91cea6e87d5f683963430d09be41fccaf1
18581023664 <451769267@qq.com>
2019-07-21 14:24:31 +0800

    添加一个second类

---
+++ git-frist-project/src/com/bdqn/git/Second.java
@@ -0,0 +1,13 @@
+package com.bdqn.git;
+
+/**
+ * @Author ldwjava
+ * @Date 2019/7/21 14:23
+ * @Desc TODO
+ **/
+
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("hello second");
+    }
+}
