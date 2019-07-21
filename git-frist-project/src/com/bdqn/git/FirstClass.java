5259ae7abadfd83ebda3e5cd78e206a07093cd44
18581023664 <451769267@qq.com>
2019-07-21 14:10:22 +0800

    把本地工程添加到git

---
+++ git-frist-project/src/com/bdqn/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.bdqn.git;
+
+/**
+ * @Author ldwjava
+ * @Date 2019/7/21 14:07
+ * @Desc TODO
+ **/
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello  xxxx");
+    }
+}
